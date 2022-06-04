bce6806a15db04b990e75dfc7285740cdbd5ae17
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-04 17:35:25 +0200

    add colos

--- main.js
+++ main.js
@@ -5,13 +5,16 @@ document.body.addEventListener('click', (e) => {
     y = e.clientY;
     
     if (x%2==0 && y%2==0) {
-        console.log("parzyste");
+        //console.log("parzyste");
+        document.body.style.backgroundColor = "#ff4757"
     }
     else if (x%2!=0 && y%2!=0) {
-        console.log("nieparzyste");
+        //console.log("nieparzyste");
+        document.body.style.backgroundColor = "#3742fa"
     }
     else {
-        console.log("mieszane");
+        //console.log("mieszane");
+        document.body.style.backgroundColor = "#2ed573"
     }
 
 })
