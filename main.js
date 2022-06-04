4e63715a1eef86eaf75c67749bfd308e40b84828
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-04 17:21:31 +0200

    add conditions

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-document.body.addEventListener('click', () => {
+document.body.addEventListener('click', (e) => {
+    console.log(e.clientX, e.clientY);
     
+    x = e.clientX;
+    y = e.clientY;
+    
+    if (x%2==0 && y%2==0) {
+        console.log("parzyste");
+    }
+    else if (x%2!=0 && y%2!=0) {
+        console.log("nieparzyste");
+    }
+    else {
+        console.log("mieszane");
+    }
 
 })
